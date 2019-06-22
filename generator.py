8cd5ee517fc3d606e63223892c8ced5b61c3a8e2
tia <yknms3533642@yahoo.co.jp>
2019-06-22 15:51:54 +0900

    genrator.py: signed型を前提にして二項演算子を実装

--- generator.py
+++ generator.py
@@ -13,9 +13,74 @@ class Generator:
             print('  pop rax')
         print('  ret')
 
-    def gen(self, node: Node):
+    def gen(self, node: Node):  # TODO: 現状: すべてsigned long型であると仮定
 
         if node.ty == ND.INT:
             if node.type.ty in ('int', 'long', 'long long'):  # 整数
                 print('  push', node.val)
                 return
+
+        # 二項演算子
+
+        self.gen(node.lhs)
+        self.gen(node.rhs)
+        print('  pop rdi')
+        print('  pop rax')
+
+        if node.ty == '*':
+            print('  imul rdi')
+
+        if node.ty == '/':
+            print('  mov rdx, 0')
+            print('  idiv rdi')
+
+        if node.ty == '%':
+            print('  mov rdx, 0')
+            print('  idiv rdi')
+            print('  push rdx')
+            return
+
+        if node.ty == '+':
+            print('  add rax, rdi')
+
+        if node.ty == '-':
+            print('  sub rax, rdi')
+
+        if node.ty == '<<':
+            print('  mov rcx, rdi')
+            print('  sal rax, cl')
+
+        if node.ty == '>>':
+            print('  mov rcx, rdi')
+            print('  sar rax, cl')
+
+        if node.ty == '<':
+            print('  cmp rax, rdi')
+            print('  setl al')
+            print('  movzx rax, al')
+
+        if node.ty == '<=':
+            print('  cmp rax, rdi')
+            print('  setle al')
+            print('  movzx rax, al')
+
+        if node.ty == '==':
+            print('  cmp rax, rdi')
+            print('  sete al')
+            print('  movzx rax, al')
+
+        if node.ty == '!=':
+            print('  cmp rax, rdi')
+            print('  setne al')
+            print('  movzx rax, al')
+
+        if node.ty == '&':
+            print('  and rax, rdi')
+
+        if node.ty == '^':
+            print('  xor rax, rdi')
+
+        if node.ty == '|':
+            print('  or rax, rdi')
+
+        print('  push rax')
